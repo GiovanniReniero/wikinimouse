fb63d790f23fddf5c9938e15e146773a6ef8cae6
Giovanni Reniero <giovanni.reniero@gmail.com>
2021-07-05 23:04:57 +0100

    added routes and index html

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,34 @@
-class ArticlesController < ApplicationController
+class ArticlesController < ApplicationControllerCRUD
+
+# READ
+  def index
+    @articles = article.all
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def new
+
+  end
+
+  def update
+
+  end
+
+  def destry
+
+  end
+
+private
+
+  def articles_params
+    params.require(:articles).(:title, :content)
+  end
+  
 end
